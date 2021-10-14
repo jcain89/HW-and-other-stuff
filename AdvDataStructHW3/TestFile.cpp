0990f81d852686929bd0a0c0458a21440924bafe
jcain89 <cainj4@mail.uc.edu>
2021-10-14 19:51:58 -0400

    First commit to new repos

---
+++ AdvDataStructHW3/TestFile.cpp
@@ -0,0 +1,15 @@
+#include<PersonClass.h>
+#include<BookClass.h>
+#include<iostream>
+#include<vecetor>
+#include<string>
+
+using namespace std;
+int main(){
+    Node* root = NULL;  //creates empty tree
+      
+     PhoneBook tree;
+     
+    
+    return 0;
+}
